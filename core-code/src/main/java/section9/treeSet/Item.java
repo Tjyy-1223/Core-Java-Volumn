bfd96b6f0fef424098ce460ba46aa3cc641ce256
tianjiangyu <161840237@nuaa.edu.cn>
2023-07-06 17:14:34 +0800

    9.2 具体的集合

---
+++ core-code/src/main/java/section9/treeSet/Item.java
@@ -0,0 +1,59 @@
+package section9.treeSet;
+
+import java.util.Objects;
+
+/**
+ * An item with a description and a part number
+ */
+public class Item implements Comparable<Item>{
+    private String description;
+    private int partNumber;
+
+    public Item(String description, int partNumber) {
+        this.description = description;
+        this.partNumber = partNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPartNumber() {
+        return partNumber;
+    }
+
+    public void setPartNumber(int partNumber) {
+        this.partNumber = partNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "description='" + description + '\'' +
+                ", partNumber=" + partNumber +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return partNumber == item.partNumber && Objects.equals(description, item.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description, partNumber);
+    }
+
+    @Override
+    public int compareTo(Item other){
+        int diff = Integer.compare(partNumber,other.partNumber);
+        return diff != 0 ? diff : description.compareTo(other.description);
+    }
+}
